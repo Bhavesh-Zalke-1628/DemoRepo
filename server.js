a3202aa5edc7c4edfb44a5b49b757eea1d4afc6b
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-04-08 12:35:38 +0530

    Second commit

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ import express from 'express'
 
 const app = express();
 
+import { Router } from 'express';
 
+const router = Router();
+
+router.get("/", (req, res) => {
+    res.send("hello")
+})
 
 app.listen(3000, () => {
     console.log("Server is runnig")
